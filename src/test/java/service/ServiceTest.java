35df3134c669f73059338bc7b3fb58170d1d1047
TattooKot <kotsagastas@gmail.com>
2021-11-14 16:29:28 +0200

    Service Test 1

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,30 +1,20 @@
 package service;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class ServiceTest {
-    private final ResultSet rs = mock(ResultSet.class);
-    private final PreparedStatement statement = mock(PreparedStatement.class);
+    private final Utils utils = Mockito.mock(Utils.class);
+    private final PreparedStatement statement = Mockito.mock(PreparedStatement.class);
 
     @Test
-    public void testGetStatement() throws SQLException {
-        Service serviceUnderTest = new Service();
-        when(statement.executeQuery(Requests.GET_ALL_LABELS.toString())).thenReturn(rs);
-        when(rs.next()).thenReturn(true);
-
-        PreparedStatement statementUnderTest = serviceUnderTest.getStatement(
-                Requests.GET_ALL_LABELS.toString());
-
-        ResultSet rsUnderTest = statementUnderTest.executeQuery();
-
-        assertEquals(rs.next(), rsUnderTest.next());
+    public void testGetStatement() {
+        when(utils.getStatement(anyString())).thenReturn(statement);
     }
 }
